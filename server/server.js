d2b4734cebc7baafcaee3f6a340260bc91e78b00
Niral Ajmera <102373223+niral261@users.noreply.github.com>
2024-09-02 12:40:09 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://blog-website-omega-kohl.vercel.app/"]
+));
 app.use(express.json());
 app.use('/', Router);
 
