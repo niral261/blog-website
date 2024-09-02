cba014c2bc46414ca7e48c63039b9b97569486b3
Niral Ajmera <102373223+niral261@users.noreply.github.com>
2024-09-03 01:00:45 +0530

    Update image-controller.js

--- server/controller/image-controller.js
+++ server/controller/image-controller.js
@@ -1,7 +1,7 @@
 import grid from 'gridfs-stream';
 import mongoose from 'mongoose';
 
-const url = ''; //http://localhost:8000
+const url = 'https://blog-website-qw7e.onrender.com'; //http://localhost:8000
 
 let gfs, gridfsBucket;
 const conn = mongoose.connection;
